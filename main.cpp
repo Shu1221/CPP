c12cc55d52bc481c9588296aaa776f1a50bc0275
Shu1221 <shusei1221shusei@gmail.com>
2021-01-15 17:54:51 +0900

    learn about def

--- main.cpp
+++ main.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+void show_value(int a)
+{
+    cout << a << endl;
+    return;
+}
+
+int add(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+
 int main()
 {
-    cout << "Hello World" << endl;
+    int x = add(10, 20);
+    show_value(x);
 }
